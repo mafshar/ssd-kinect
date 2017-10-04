1880722df4b9a447ce279de25bbf0fdfa291d2b8
Mohammad Afshar <ma2510@nyu.edu>
2017-10-03 21:15:06 -0400

    skeloton code for setting up kinect

--- runner.py
+++ runner.py
@@ -7,17 +7,24 @@ import os
 import sys
 import numpy as np
 import signal
-# import cv
+import cv2
+from time import time
 
 ## write code to get take the kinect images and then convert frames to tf records
 ## tf records then get fed into the tf-models object recognition pipeline
 
 KEEP_RUNNING = True
 
-def run():
-    while KEEP_RUNNING:
-        pass
 
+def run():
+    start = time()
+    while KEEP_RUNNING:
+        rgb, _ = get_video()
+        d3 = np.dstack((r,g,depth/20)).astype(np.uint8)
+        da = np.hstack((d3,rgb[::2, ::2]))
+        ## downsample image and then
+        cv2.imshow('image', da)
+        cv2.waitKey(0)
     return
 
 if __name__ == '__main__':
